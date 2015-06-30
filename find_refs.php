149a2fa45c658fd0ccdc9273e5c7fb22103912da
Moritz Wilhelmy <m.wilhelmy@bgcc.at>
2015-06-30 15:17:08 +0200

    implement recursive tree walking

--- find_refs.php
+++ find_refs.php
@@ -3,10 +3,7 @@
 // See https://github.com/nikic/PHP-Parser/blob/1.x/doc/2_Usage_of_basic_components.markdown
 
 require 'vendor/autoload.php';
-
 ini_set('xdebug.max_nesting_level', 3000);
-
-
 use PhpParser\Node;
 
 class NodeVisitor extends PhpParser\NodeVisitorAbstract
@@ -66,20 +63,38 @@ foreach (array_slice($argv,1) as $arg) {
 		$OPTIONS['dump'] = true;
 		continue;
 	}
-	# TODO add RecursiveIteratorIterator with RegexIterator('/\.php$/') here...
+	if ($arg === "-r" || $arg == "--recurse") {
+		$OPTIONS['recurse'] = true;
+		continue;
+	}
 	if ($arg === "-h" || $arg == "--help") {
 		echo "{$argv[0]} [--help|-h] [--dump|-d] file1.php ...\n\n";
 		echo "--help|-h      print this message\n";
 		echo "--dump|-d      dump AST instead of trying to lint it\n";
+		echo "--recurse|-r   Search directories in command line arguments for files ending in .php recursively and lint them all.\n";
 		continue;
 	}
-	
+
+	if ($OPTIONS['recurse']) {
+		$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($arg));
+		$files = new RegexIterator($files, '/\.php$/');
+		foreach ($files as $file) {
+			$code = @file_get_contents($file);
+			if ($code === false)
+				$errors[] = "Could not open input file $file";
+			else
+				$source[(string)$file] = $code;
+		}
+		continue;
+	}
+
 	$code = @file_get_contents($arg);
 	if ($code === false)
 		$errors[] = "Could not open input file $arg";
 	else
 		$source[$arg] = $code;
 }
+
 
 if (sizeof($errors) !== 0) {
 	file_put_contents('php://stderr', implode($errors,"\n")."\nAborted.\n");
